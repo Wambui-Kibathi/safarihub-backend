1c36ed9ad6514baf3b8b67c114d9a5a1f7a5cc65
derrickgabe <derrickg844@gmail.com>
2025-10-30 12:56:53 +0300

    Finalizing git init

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 from .user import User
 from .destination import Destination
 from .payment import Payment
 
-__all__ = ['db', 'User', 'Destination', 'Payment']
+class BaseModel(db.Model):
+    __abstract__ = True
+    
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+__all__ = ['db', 'migrate', 'BaseModel', 'User', 'Destination', 'Payment']
